78d3c5f18b4546c0aba79433ec7d6d4ff3c34617
SanqDuonq <namsang0902s@gmail.com>
2024-12-07 16:12:27 +0700

    Connect database

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 import express from 'express'
 import log from './utils/logger';
 import dotenv from 'dotenv';
+import NotFoundRoute from './middlewares/not-found-route';
+import connectMongoDB from './database/connect-mongoDB';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/',(req,res,next) => {
+    res.send('Home Page')
+});
+app.use(NotFoundRoute);
+
+
 app.listen(port,() => {
-    log.info(`App started at http://localhost:${port}`)
+    log.info(`App started at http://localhost:${port}`);
+    connectMongoDB();
 })
