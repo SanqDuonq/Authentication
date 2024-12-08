4c35b07ac061209067e84a9bd35d85433a20d12f
SanqDuonq <namsang0902s@gmail.com>
2024-12-08 20:22:18 +0700

    Reset password feature

--- controllers/auth.controllers.ts
+++ controllers/auth.controllers.ts
@@ -1,8 +1,9 @@
-import { Request,Response } from "express";
+import { Request, Response } from 'express';
 import { userInput } from "../utils/validate";
 import services from '../services/auth.services';
 import sendVerifyEmail from "../services/email.services";
 import { verifyEmailInput } from "../schema/user.schema";
+import User from '../models/user.model';
 
 async function signup(req:Request<userInput>,res:Response) {
     const body = req.body;
@@ -24,9 +25,22 @@ async function verifyEmail(req:Request<verifyEmailInput>,res:Response) {
         res.status(200).json({
             message: 'Verify email is successful'
         })
-    } catch (error) {
-        res.status(500).json(error);
+    } catch (error:any) {
+        res.status(500).json(error.message);
     }
+}
+
+async function resetPassword(req:Request,res: Response) {
+    const email = req.body.email;
+    const user = await services.findUserByEmail(email);
+    if (!user) {
+        res.status(400).json({
+            message: 'Email not found!'
+        })
+    }
+    res.status(200).json({
+        message: `Verification code sent to ${email}`
+    })
 }
 
 
@@ -37,5 +51,6 @@ async function login(req:Request,res:Response){
 export default {
     signup,
     login,
-    verifyEmail
+    verifyEmail,
+    resetPassword
 }
